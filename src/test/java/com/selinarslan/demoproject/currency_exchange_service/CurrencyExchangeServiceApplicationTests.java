8e34efc1799c431695150fa4119e78686342afff
Selin Leblebicioğlu <selinlb37@gmail.com>
2026-06-06 22:16:14 +0300

    first commit

---
+++ src/test/java/com/selinarslan/demoproject/currency_exchange_service/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selinarslan.demoproject.currency_exchange_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
